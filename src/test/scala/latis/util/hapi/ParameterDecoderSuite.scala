2ce45a5de3276f3a6da751db33f443579760119e
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2022-05-24 13:19:54 -0600

    cleanup

--- src/test/scala/latis/util/hapi/ParameterDecoderSuite.scala
+++ src/test/scala/latis/util/hapi/ParameterDecoderSuite.scala
@@ -6,57 +6,46 @@ class ParameterDecoderSuite extends JsonDecoderSuite {
 
   test("accept and decode scalar parameters") {
     withJsonResource("data/hapi-parameter-scalar.json") {
-      decodedAs(_) { param: Parameter =>
-        param match {
-          case ScalarParameter(name, tyName, units, length, fill) =>
-            assertEquals(name, "Np")
-            assertEquals(tyName, "double")
-            assertEquals(units, "#/cc")
-            assertEquals(length, None)
-            assertEquals(fill, Some("-1.0E31"))
-          case _ => fail("Decoded to wrong parameter type.")
-        }
+      decodedAs[Parameter](_) {
+        case ScalarParameter(name, tyName, units, length, fill) =>
+          assertEquals(name, "Np")
+          assertEquals(tyName, "double")
+          assertEquals(units, "#/cc")
+          assertEquals(length, None)
+          assertEquals(fill, Some("-1.0E31"))
+        case _ => fail("Decoded to wrong parameter type.")
       }
     }
   }
 
   test("accept and decode vector parameters") {
     withJsonResource("data/hapi-parameter-vector.json") {
-      decodedAs(_) { param: Parameter =>
-        param match {
-          case VectorParameter(name, tyName, units, length, fill, size) =>
-            assertEquals(name, "V_GSE")
-            assertEquals(tyName, "double")
-            assertEquals(units, "km/s")
-            assertEquals(length, None)
-            assertEquals(fill, Some("-1.0E31"))
-            assertEquals(size, 3)
-          case _ => fail("Decoded to wrong parameter type.")
-        }
+      decodedAs[Parameter](_) {
+        case VectorParameter(name, tyName, units, length, fill, size) =>
+          assertEquals(name, "V_GSE")
+          assertEquals(tyName, "double")
+          assertEquals(units, "km/s")
+          assertEquals(length, None)
+          assertEquals(fill, Some("-1.0E31"))
+          assertEquals(size, 3)
+        case _ => fail("Decoded to wrong parameter type.")
       }
     }
   }
 
   test("accept and decode parameters with a single bin") {
     withJsonResource("data/hapi-parameter-array.json") {
-      decodedAs(_) { param: Parameter =>
-        param match {
-          case ArrayParameter(name, tyName, units, length, fill, size, bin) =>
-            assertEquals(name, "proton_spectrum_uncerts")
-            assertEquals(tyName, "double")
-            assertEquals(units, "particles/(sec ster cm^2 keV)")
-            assertEquals(length, None)
-            assertEquals(fill, Some("-1e31"))
-            assertEquals(size, 3)
-
-            bin match {
-              case Bin(name, units) =>
-                assertEquals(name, "energy")
-                assertEquals(units, "keV")
-              case _ => fail("Decoded bin incorrectly")
-            }
-          case _ => fail("Decoded to wrong parameter type.")
-        }
+      decodedAs[Parameter](_) {
+        case ArrayParameter(name, tyName, units, length, fill, size, Bin(binName, binUnits)) =>
+          assertEquals(name, "proton_spectrum_uncerts")
+          assertEquals(tyName, "double")
+          assertEquals(units, "particles/(sec ster cm^2 keV)")
+          assertEquals(length, None)
+          assertEquals(fill, Some("-1e31"))
+          assertEquals(size, 3)
+          assertEquals(binName, "energy")
+          assertEquals(binUnits, "keV")
+        case _ => fail("Decoded to wrong parameter type.")
       }
     }
   }
